3660a2efd2539727e6bf15841bb1b9cb53793d4e
DamianJaszewski <DamianJaszewski@users.noreply.github.com>
2020-07-20 16:05:42 +0200

    Dodaj pliki projektów.

---
+++ Siesta/formMain.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Siesta
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 470);
+            this.Name = "formMain";
+            this.Text = "Siesta";
+            this.Load += new System.EventHandler(this.formMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
