a971960da17323ee3fbe7c18d09b88dfbad23467
DamianJaszewski <DamianJaszewski@users.noreply.github.com>
2020-07-21 14:34:30 +0200

    commi

--- Siesta/formMain.Designer.cs
+++ Siesta/formMain.Designer.cs
@@ -28,21 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelRecipes = new System.Windows.Forms.Label();
+            this.listRecipes = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // labelRecipes
+            // 
+            this.labelRecipes.AutoSize = true;
+            this.labelRecipes.Location = new System.Drawing.Point(22, 26);
+            this.labelRecipes.Name = "labelRecipes";
+            this.labelRecipes.Size = new System.Drawing.Size(46, 13);
+            this.labelRecipes.TabIndex = 0;
+            this.labelRecipes.Text = "Recipes";
+            // 
+            // listRecipes
+            // 
+            this.listRecipes.FormattingEnabled = true;
+            this.listRecipes.Location = new System.Drawing.Point(25, 57);
+            this.listRecipes.Name = "listRecipes";
+            this.listRecipes.Size = new System.Drawing.Size(120, 95);
+            this.listRecipes.TabIndex = 1;
+            this.listRecipes.SelectedIndexChanged += new System.EventHandler(this.listRecipes_SelectedIndexChanged);
             // 
             // formMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(852, 470);
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.listRecipes);
+            this.Controls.Add(this.labelRecipes);
             this.Name = "formMain";
-            this.Text = "Siesta";
-            this.Load += new System.EventHandler(this.formMain_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private SiestaDataSet siestaDataSet;
+        private SiestaDataSet siestaDataSet1;
+        private System.Windows.Forms.Label lblRecipes;
+        private System.Windows.Forms.ListBox lisRecipes;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label asdf;
+        private System.Windows.Forms.Label labelRecipes;
+        private System.Windows.Forms.ListBox listRecipes;
     }
 }
 
--- Siesta/formMain.cs
+++ Siesta/formMain.cs
@@ -7,19 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace Siesta
 {
     public partial class formMain : Form
     {
+        SqlConnection connection;
+        string connectionString;
+
         public formMain()
         {
             InitializeComponent();
+
+            connectionString = ConfigurationManager.ConnectionStrings["Siesta.Properties.Settings.SiestaConnectionString"].ConnectionString;
+        }
+        private void listRecipes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopularRecipes();
         }
 
-        private void formMain_Load(object sender, EventArgs e)
+        private void PopularRecipes()
         {
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Recipe", connection))
 
+            {
+                DataTable recipeTable = new DataTable();
+                adapter.Fill(recipeTable);
+
+                listRecipes.DisplayMember = "Name";
+                listRecipes.ValueMember = "Id";
+                listRecipes.DataSource = recipeTable;
+            }
+           
         }
     }
 }
