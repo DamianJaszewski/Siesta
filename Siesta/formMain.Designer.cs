383beb820d0c2d3f1af51d31b92b1dcba41833b2
DamianJaszewski <DamianJaszewski@users.noreply.github.com>
2020-07-23 14:25:23 +0200

    Add Ingredients Table

--- Siesta/formMain.Designer.cs
+++ Siesta/formMain.Designer.cs
@@ -30,7 +30,9 @@
         {
             this.siestaDataSet4 = new Siesta.SiestaDataSet();
             this.lblRecipe = new System.Windows.Forms.Label();
-            this.lstRecipe1 = new System.Windows.Forms.ListBox();
+            this.lstRecipe = new System.Windows.Forms.ListBox();
+            this.lstIngredient = new System.Windows.Forms.ListBox();
+            this.lblIngredient = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.siestaDataSet4)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,18 +48,39 @@
             this.lblRecipe.Size = new System.Drawing.Size(123, 28);
             this.lblRecipe.TabIndex = 0;
             this.lblRecipe.Text = "Recipe";
+            this.lblRecipe.Click += new System.EventHandler(this.lblRecipe_Click);
             // 
-            // lstRecipe1
+            // lstRecipe
             // 
-            this.lstRecipe1.Location = new System.Drawing.Point(36, 74);
-            this.lstRecipe1.Name = "lstRecipe1";
-            this.lstRecipe1.Size = new System.Drawing.Size(120, 95);
-            this.lstRecipe1.TabIndex = 0;
+            this.lstRecipe.Location = new System.Drawing.Point(36, 74);
+            this.lstRecipe.Name = "lstRecipe";
+            this.lstRecipe.Size = new System.Drawing.Size(120, 95);
+            this.lstRecipe.TabIndex = 0;
+            this.lstRecipe.SelectedIndexChanged += new System.EventHandler(this.lstRecipe_SelectedIndexChanged);
+            // 
+            // lstIngredient
+            // 
+            this.lstIngredient.Location = new System.Drawing.Point(165, 74);
+            this.lstIngredient.Name = "lstIngredient";
+            this.lstIngredient.Size = new System.Drawing.Size(120, 95);
+            this.lstIngredient.TabIndex = 1;
+            this.lstIngredient.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // lblIngredient
+            // 
+            this.lblIngredient.Location = new System.Drawing.Point(162, 43);
+            this.lblIngredient.Name = "lblIngredient";
+            this.lblIngredient.Size = new System.Drawing.Size(123, 28);
+            this.lblIngredient.TabIndex = 2;
+            this.lblIngredient.Text = "Ingredients";
+            this.lblIngredient.Click += new System.EventHandler(this.label1_Click);
             // 
             // formMain
             // 
-            this.ClientSize = new System.Drawing.Size(420, 317);
-            this.Controls.Add(this.lstRecipe1);
+            this.ClientSize = new System.Drawing.Size(525, 396);
+            this.Controls.Add(this.lstIngredient);
+            this.Controls.Add(this.lblIngredient);
+            this.Controls.Add(this.lstRecipe);
             this.Controls.Add(this.lblRecipe);
             this.Name = "formMain";
             this.Load += new System.EventHandler(this.formMain_Load);
@@ -70,7 +93,9 @@
 
         private SiestaDataSet siestaDataSet4;
         private System.Windows.Forms.Label lblRecipe;
-        private System.Windows.Forms.ListBox lstRecipe1;
+        private System.Windows.Forms.ListBox lstRecipe;
+        private System.Windows.Forms.ListBox lstIngredient;
+        private System.Windows.Forms.Label lblIngredient;
     }
 }
 
--- Siesta/formMain.cs
+++ Siesta/formMain.cs
@@ -25,14 +25,10 @@ namespace Siesta
         }
         private void formMain_Load(object sender, EventArgs e)
         {
-            PopularRecipes();
-        }
-        private void listRecipes_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            //PopularRecipes();
+            PopulateRecipes();
         }
 
-        private void PopularRecipes()
+        private void PopulateRecipes()
         {
             using (connection = new SqlConnection(connectionString))
             using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Recipe", connection))
@@ -41,10 +37,52 @@ namespace Siesta
                 DataTable recipeTable = new DataTable();
                 adapter.Fill(recipeTable);
 
-                lstRecipe1.DisplayMember = "Name";
-                lstRecipe1.ValueMember = "Id";
-                lstRecipe1.DataSource = recipeTable;
+                lstRecipe.DisplayMember = "Name";
+                lstRecipe.ValueMember = "Id";
+                lstRecipe.DataSource = recipeTable;
             }
+
+        }
+        private void PopulateIngredient()
+        {
+            string query = "SELECT a.Name FROM Ingredient a " +
+                "INNER JOIN RecipeIndex b ON a.Id=b.IngredientId " +
+                "WHERE b.RecipeId = @RecipeId";
+
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command)) 
+
+            {
+                command.Parameters.AddWithValue("@RecipeId", lstRecipe.SelectedValue);
+
+                DataTable ingredientTable = new DataTable();
+                adapter.Fill(ingredientTable);
+
+                lstIngredient.DisplayMember = "Name";
+                lstIngredient.ValueMember = "Id";
+                lstIngredient.DataSource = ingredientTable;
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstRecipe_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopulateIngredient();
+        }
+
+        private void lblRecipe_Click(object sender, EventArgs e)
+        {
 
         }
     }
